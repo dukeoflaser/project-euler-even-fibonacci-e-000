76ad25a2036ad23442d9cc704bee6756e62b41d7
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-01 18:47:00 +0000

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+require 'pry'
+require 'pry-nav'
+
+def even_fibonacci_sum(limit)
+  sequence = [1]
+  sequence.each_with_index do |n, i|
+    i == 0 ? sum = n : sum = n + sequence[i - 1]
+    sequence << sum  if limit > sum
+  end
+
+  evens = sequence.find_all { |n| n % 2 == 0 }
+  evens.inject { |sum, n| sum + n }
+end
+
+
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+  
+  def fib_sequence
+    sequence = [1]
+    sequence.each_with_index do |n, i|
+      i == 0 ? sum = n : sum = n + sequence[i - 1]
+      sequence << sum  if @limit > sum  
+    end
+  end
+  
+  def sum
+    evens = fib_sequence.find_all { |n| n % 2 == 0 }
+    evens.inject { |sum, n| sum + n }
+  end
+end
